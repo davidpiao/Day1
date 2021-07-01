5b05187bd09b99cd79f4722bd71494068639055a
churry97 <63008958+churry97@users.noreply.github.com>
2021-07-01 11:15:35 +0900

    HelloWorld!

--- app/src/main/java/com/example/helloworlds/MainActivity.java
+++ app/src/main/java/com/example/helloworlds/MainActivity.java
@@ -6,11 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //on create 자동으로 불림.
-    //이건 Main 함수라고 불리면 된다.
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //이건 Main 함수라고 불리면 된다.
         super.onCreate(savedInstanceState);  //부모 부르기
-        setContentView(R.layout.activity_main); //그 레이아웃 가져오기
+        setContentView(R.layout.layout2); //그 레이아웃 가져오기
     }
 }
